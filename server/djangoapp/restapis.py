1b0b5c2c42e7243792a15dcbec42a5194d3ffdb8
bashlogs <mayurkhadde49@gmail.com>
2024-10-17 02:50:35 -0400

    kubernetes

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,7 +1,8 @@
 # Uncomment the imports below before you add the function code
-import requests
+# import requests
 import os
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 
@@ -29,7 +30,7 @@ def get_request(endpoint, **kwargs):
         print("Network exception occurred")
 
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"analyze/"+text
+    request_url = sentiment_analyzer_url+"/analyze/"+text
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
